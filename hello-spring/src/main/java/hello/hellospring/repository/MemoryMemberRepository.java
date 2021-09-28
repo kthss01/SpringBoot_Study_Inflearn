7ec2ca4f6fd9bb9930437ca57e00da3e1fde8416
kthss01 <kthss01@naver.com>
2021-09-28 22:55:57 +0900

    스프링 웹 개발 기초, 회원 관리 예제, 테스트 해보기 (JUnit)

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,41 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    public static Map<Long, Member> store = new HashMap<>(); // 실무에선 ConcurrentHashMap
+    public static long sequence = 0L; // 동시성 문제를 생각해서 atomic long을 해야함
+
+    @Override
+    public Member save(Member member) {
+        // name은 이미 member에 들어 있음
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // null이어도 가능
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        ArrayList<Member> members = new ArrayList<>(store.values());
+        return members;
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,54 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+//        Optional<Member> result = memberRepository.findByName(member.getName()); // 이렇게 꺼내는거 별로라고함
+////        Member member1 = result.get(); // 이렇게 바로 꺼내는건 추천 x orElse 이런거 쓴다고함
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다");
+//        });
+
+        // 이렇게 사용하면 깔끔
+        // 이런건 메소드로 뽑는게 좋다고 하심
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다");
+                        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
