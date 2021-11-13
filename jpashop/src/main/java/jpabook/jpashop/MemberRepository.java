78bd9849969bc071724b1add93e503a7ec2a5dc4
kthss01 <kthss01@naver.com>
2021-11-14 08:05:58 +0900

    회원 도메인 개발

--- jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -6,18 +6,18 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@Repository
+//@Repository
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
-
-    public Long save(Member member) {
-        em.persist(member);
-        return member.getId();
-    }
-
-    public Member find(Long id) {
-        return em.find(Member.class, id);
-    }
+//    @PersistenceContext
+//    private EntityManager em;
+//
+//    public Long save(Member member) {
+//        em.persist(member);
+//        return member.getId();
+//    }
+//
+//    public Member find(Long id) {
+//        return em.find(Member.class, id);
+//    }
 }
